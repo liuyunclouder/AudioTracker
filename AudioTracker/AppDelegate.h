14d44ec04f3f3c6c4e0b9094708f7cf5f754decf
灵吾 <liuyunclouder@gmail.com>
2016-04-25 14:36:43 +0800

    Initial Commit

---
+++ AudioTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioTracker
+//
+//  Created by TangYunfei on 16/4/25.
+//  Copyright © 2016年 TangYunfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioTracker
+//
+//  Created by TangYunfei on 16/4/25.
+//  Copyright © 2016年 TangYunfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
